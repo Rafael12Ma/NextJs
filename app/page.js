91c6f483669b91d2fd78a3a91a859ffa1618c1e6
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2024-06-09 22:06:12 +0300

    Test commit

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <h1>Welcome to this NextJS Course!TEEEST</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
     </main>
   );
