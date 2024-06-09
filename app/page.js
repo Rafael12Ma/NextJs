0b48d994b71290c5d2b8f23168e98374df725a31
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2024-06-09 22:15:43 +0300

    Test after remove remote

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Coursee!TEEEST</h1>
+      <h1>Welcome to NextJS Coursee!TEEEST</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
     </main>
   );
